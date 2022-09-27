ac9edf29d894e905ecebbf629217bbc0eb094158
valentinafv96 <64854217+valentinafv96@users.noreply.github.com>
2022-09-27 18:53:08 -0500

    Update test_chiralsol.py

--- tests/test_chiralsol.py
+++ tests/test_chiralsol.py
@@ -1,18 +1,17 @@
 import unittest
-# from chiral import chiralsol
+from chiral import chiralsol
 
 
 class Test_chiralsolone(unittest.TestCase):
     def test__working(self):
-          self.assertEqual(1,1,True)
-#         self.assertEqual(chiralsol.paralelo(5, None, None, None, None, None),
-#                          11, True)
+#           self.assertEqual(1,1,True)
+        self.assertEqual(chiralsol.paralelo(5, None, None, None, None, None),
+                         11, True)
 
-
-# class Test_chiralsoltwo(unittest.TestCase):
-#     def test__working(self):
-#         self.assertEqual(chiralsol.paralelo(6, None, None, None, None, None),
-#                          112, True)
+class Test_chiralsoltwo(unittest.TestCase):
+    def test__working(self):
+        self.assertEqual(chiralsol.paralelo(6, None, None, None, None, None),
+                         112, True)
 
 if __name__ == '__main__':
     unittest.main()

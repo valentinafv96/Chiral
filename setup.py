3f73105f1a000d55ce292b65b90a369fda29b384
valentinafv96 <64854217+valentinafv96@users.noreply.github.com>
2022-09-28 17:49:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="chiralsol",
 
         # Version number (initial):
-        version="0.0.1",
+        version="0.0.5",
 
         # Application author details:
         author="Valentina",

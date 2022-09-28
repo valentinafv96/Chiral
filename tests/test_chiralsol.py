02ef68aaa1d828786bd1787f5aa2cccd0cb4d91e
valentinafv96 <64854217+valentinafv96@users.noreply.github.com>
2022-09-28 10:45:35 -0500

    Update test_chiralsol.py

--- tests/test_chiralsol.py
+++ tests/test_chiralsol.py
@@ -4,13 +4,13 @@ from chiral import chiralsol
 
 class Test_chiralsolone(unittest.TestCase):
     def test__working(self):
-        self.assertEqual(chiralsol.paralelo(5),
+        self.assertEqual(chiralsol.paralelo(5,1000000,9,30,None,'soluciones'),
                          11, True)
 
 
 class Test_chiralsoltwo(unittest.TestCase):
     def test__working(self):
-        self.assertEqual(chiralsol.paralelo(6),
+        self.assertEqual(chiralsol.paralelo(6,1000000,9,30,None,'soluciones'),
                          112, True)
 
 
